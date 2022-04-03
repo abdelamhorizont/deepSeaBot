7f9f43317859ee41a26978639e3f00c7a5893e19
abdelamhorizont <abdelhamid.ameur@hotmail.de>
2022-04-03 12:41:46 +0200

    p5 works

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -4,12 +4,13 @@ import Sketch from "react-p5";
 const Canvas = (props) => {
 
 	const setup = (p5, canvasParentRef) => {
-		p5.createCanvas(500, 500).parent(canvasParentRef);
+		p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+        // p5.background(0, 0, 0);
 	};
 
 	const draw = (p5) => {
-		p5.background(100, 100, 100, 1);
-		// p5.ellipse(p5.mouseX, p5.mouseY, 70, 70);
+		p5.background(0, 0, 0);
+		p5.ellipse(p5.mouseX, p5.mouseY, 70, 70);
 	};
 
 	return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import * as React from "react"
 import Canvas from '../components/canvas/canvas'
 
+import './reset.css'
+
 // markup
 const IndexPage = () => {
   return (
