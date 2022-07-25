b5fca5d8a40b83e8e871c4998f4e677ba3cb2d8d
abdelamhorizont <abdelhamid.ameur@hotmail.de>
2022-07-25 21:55:40 +0200

    no nothing

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -5,29 +5,29 @@ import rosa from './rosa2.png'; // Tell webpack this JS file uses this image
 const Canvas = () => {
 	let rosaBrush;
 
-	// const isBrowser = () => typeof window !== "undefined"
-	// const windowWitdh = isBrowser() && window.screen.width
-	// const windowheight = isBrowser() && window.screen.height
+	const isBrowser = () => typeof window !== "undefined"
+	const windowWitdh = isBrowser() && window.screen.width
+	const windowheight = isBrowser() && window.screen.height
 
-    const preload = (p5) => {
-		// rosaBrush = p5.loadImage('./rosa2.png');
-    }
+   //  const preload = (p5) => {
+	// 	rosaBrush = p5.loadImage('./rosa2.png');
+   //  }
 
 	const setup = (p5, canvasParentRef) => {
-		// p5.createCanvas(windowWitdh, windowheight).parent(canvasParentRef);
-      //   p5.colorMode(p5.HSB);
+		p5.createCanvas(windowWitdh, windowheight).parent(canvasParentRef);
+        p5.colorMode(p5.HSB);
 	};
 
 	const draw = (p5) => {
-		// p5.background(0);
+		p5.background(0);
 
-		// p5.ellipse(p5.mouseX, p5.mouseY, 70, 70);
+		p5.ellipse(p5.mouseX, p5.mouseY, 70, 70);
 		// p5.image(rosaBrush, p5.mouseX, p5.mouseY, 200, 200);
 	};
 
 	return (
 		<div style={{position : "fixed", top : "0rem", zIndex : "1"}}>
-			<Sketch setup={setup} draw={draw} />
+			{/* <Sketch setup={setup} draw={draw} /> */}
 		</div>
 	)
 };
