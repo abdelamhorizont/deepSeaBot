a471bd49f62a8d1d16dbf6533ecb8758a474b124
abdelamhorizont <abdelhamid.ameur@hotmail.de>
2022-12-14 13:12:17 +0100

    draw shapes

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { useRef, useEffect, useState } from "react";
-// import Sketch from "react-p5";
+// import { motion } from "framer-motion"
+
 import rosa from './rosa2.png'; // Tell webpack this JS file uses this image
+import './canvas.css'
 
 import Loadable from "@loadable/component"
-
 const Sketch = Loadable(
 	() => import("react-p5")
 )
+
+const { motion } = require("framer-motion");
 
 const isBrowser = () => typeof window !== "undefined"
 const windowWitdh = isBrowser() && window.innerWidth
@@ -18,8 +21,17 @@ export default function Canvas() {
 	const img = useRef(null)
 	const brushDivs = useRef(null)
 	let cnv;
-	let incr = 0
-	// let osc
+
+	const [cursor, setCursor] = useState({
+		x: 0,
+		y: 0
+	});
+
+	const [shape, setShape] = useState({
+		positions: []
+	})
+
+	const [shapes, setShapes] = useState([shape])
 
 	const [brush, setBrush] = useState({
 		color: rosa,
@@ -30,52 +42,19 @@ export default function Canvas() {
 		}
 	})
 
-	let positions = [1, 2, 3, 4]
-
-	// let brush = {
-	// 	color: rosa,
-	// 	positions: [],
-	// 	osc: {
-	// 		x: 0,
-	// 		y: 0
-	// 	}
-	// }
-
-	// const [mouse, setMouse] = useState({
-	// 	positions: []
-	// })
-
-	let mouse = {
-		positions: []
-	}
-
 	const setup = (p5, canvasParentRef) => {
 		cnv = p5.createCanvas(windowWitdh, windowheight).parent(canvasParentRef)
 		p5.colorMode(p5.HSB)
 		p5.rectMode(p5.CENTER)
-		brush.rosa = p5.loadImage(rosa)
-		// console.log(img.current)
-
+		// brush.rosa = p5.loadImage(rosa)
 	}
 
 	const draw = (p5) => {
-		p5.background(0)
-
-		cursor(p5)
+		p5.background(0, 0)
 		drawing(p5)
 	}
 
-	const cursor = (p5) => {
-		// p5.imageMode(p5.CENTER);
-		// p5.image(brush.rosa, p5.mouseX, p5.mouseY, 200, 200);
-		img.current.style = "position: fixed; top:" + (p5.mouseY - img.current.offsetHeight / 2) + "px; left:" + (p5.mouseX - img.current.offsetWidth / 2) + "px;"
-		// console.log(brushDivs.current)
-		// brushDivs.current.style = "position: fixed; top:" + 100 + "px; left:" + 100 + "px;"
-
-	}
-
 	const drawing = (p5) => {
-
 		// brush.positions = [...mouse.positions]
 		// brush.positions.map((pos) => {
 		// p5.image(brush.rosa, pos.x + brush.osc.x, pos.y + brush.osc.y, 200, 200)
@@ -94,13 +73,22 @@ export default function Canvas() {
 	}
 
 	const mouseDragged = (p5) => {
-		mouse.positions.push({ x: p5.mouseX, y: p5.mouseY })
-		setBrush(prevState => ({
-			...prevState,
-			positions: [...prevState.positions, ...mouse.positions]
-		}))
+		setShape({
+			positions: [...shape?.positions, {
+				x: p5.mouseX,
+				y: p5.mouseY
+			}]
+		})
+	}
 
-		console.log(brush)
+	const mouseReleased = (p5) => {
+		// const shapeCopy = JSON.parse(JSON.stringify(shapes));
+		setShape({
+			positions: []
+		})
+
+		setShapes([...shapes, shape])
+		console.log(shapes);
 	}
 
 	class NoiseLoop {
@@ -120,28 +108,86 @@ export default function Canvas() {
 		}
 	}
 
-	const divStyle = { 
-		background: "radial-gradient(farthest-side at 50% 50%, pink, green, transparent",		
-		width: "100px", 
-		height: "100px" 
+	const cursorStyle = {
+		transform: `translate(${cursor.x - 50}px, ${cursor.y - 50}px)`,
+		background: "radial-gradient(farthest-side at 50% 50%, blue, transparent",
+		width: "100px",
+		height: "100px",
+	}
+
+	const motionstyle = {
+		background: "radial-gradient(farthest-side at 50% 50%, blue, transparent",
+		width: "100px",
+		height: "100px",
+		position: "absolute",
 	}
 
 	return (
-		<div style={{ position: "fixed", top: "0rem", zIndex: "1" }}>
-			<div ref={img}><div style={divStyle}></div></div>
+		// <div style={{ position: "fixed", top: "0rem", zIndex: "1" }}>
+		<div onPointerMove={e => {
+			setCursor({
+				x: e.clientX,
+				y: e.clientY
+			})
+		}}
+			style={{
+				position: 'relative',
+				width: '100vw',
+				height: '100vh',
+				backgroundColor: "black",
+				cursor: "none"
+			}}
+		>
+			{/* framer motio test */}
+			{/* <motion.div animate={{ x: 100 }}>
+				<div style={motionstyle}></div>
+			</motion.div> */}
+
+			{/* cursor brush style */}
+			<div style={cursorStyle}></div>
+
+			{/* svg drawings */}
+
+			<div style={{ position: "fixed", top: "0rem", zIndex: "1" }}>
+				<svg height={windowheight} width={windowWitdh} fill="yellow">
+					<path d={"M" + shape.positions[0]?.x + " " + shape.positions[0]?.y + "L" +
+						shape.positions?.map((pos) =>
+							+ pos.x + " " + pos.y
+						)
+						+ " Z"} />
+				</svg>
+			</div>
 
 			{
-				brush.positions.map((pos) =>
-					<div ref={brushDivs} style={{ position: "fixed", top: (pos.y - 50) + "px", left: (pos.x - 50) + "px", zIndex: "3" }}>
-						<div style={divStyle}></div>
-					</div>
-				)
+				shapes.map((shape) => {
+					return (
+						<div style={{ position: "fixed", top: "0rem", zIndex: "1" }}>
+							<svg height={windowheight} width={windowWitdh} fill="yellow">
+								<path d={"M" + shape.positions[0]?.x + " " + shape.positions[0]?.y + "L" +
+									shape.positions?.map((pos) =>
+										+ pos.x + " " + pos.y
+									)
+									+ "Z"} />
+							</svg>
+						</div>
+					)
+				})
 			}
 
+			{/* {
+				brush.positions.map((pos) =>
+					<div className="brush" ref={brushDivs} style={{ position: "fixed", top: (pos.y - 50) + "px", left: (pos.x - 50) + "px", zIndex: "3" }}>
+						<div style={cursorStyle}></div>
+					</div>
+				)
+			} */}
+
 			<Sketch
+				style={{ position: "fixed", top: "0rem", zIndex: "1" }}
 				setup={setup}
 				draw={draw}
 				mouseDragged={mouseDragged}
+				mouseReleased={mouseReleased}
 			/>
 		</div>
 	)
